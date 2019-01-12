f7a73e380cfc438a72dc1649b5d3f6671e1aaa69
Yash Melkani <yashmelkani02@gmail.com>
2019-01-11 19:01:49 -0800

    Basic DriveTrain

---
+++ src/main/include/Subsystems/DriveTrain.h
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <Commands/Subsystem.h>
+#include "ctre/Phoenix.h"
+#include "WPILib.h"
+
+class DriveTrain : public frc::Subsystem {
+ private:
+ TalonSRX* left;
+ TalonSRX* right;
+   // It's desirable that everything possible under private except
+  // for methods that implement subsystem capabilities
+
+ public:
+  DriveTrain();
+  void tankDrive(double leftPow, double rightPow);
+  //void moveArm(double armPower);
+  void InitDefaultCommand() override;
+};
